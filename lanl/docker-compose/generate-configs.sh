6fb0eb6906696f59d9f62ae77eded8d9024544c2
Travis Cotton <trcotton@lanl.gov>
2024-01-30 12:50:55 -0700

    fixing sed to work on both GNU and MAC

--- lanl/docker-compose/generate-configs.sh
+++ lanl/docker-compose/generate-configs.sh
@@ -20,6 +20,6 @@ cp configs/ochami-template.yaml $OCHAMI_CONFIG
 
 #replace passwords in template. 
 #The sed delimeter is '@' because randomly generated passwds can have slashes and sed doesn't like that
-SMDDBPASSWD="$(openssl rand -base64 32)"; sed -i "s@SMD_DB_PASSWD@$SMDDBPASSWD@" $OCHAMI_CONFIG
-BSSDBPASSWD="$(openssl rand -base64 32)"; sed -i "s@BSS_DB_PASSWD@$BSSDBPASSWD@" $OCHAMI_CONFIG
-HYDRADBPASSWD="$(openssl rand -base64 32)"; sed -i "s@HYDRA_DB_PASSWD@$HYDRADBPASSWD@" $OCHAMI_CONFIG
+SMDDBPASSWD="$(openssl rand -base64 32)"; sed -i'' -e "s@SMD_DB_PASSWD@$SMDDBPASSWD@" "${OCHAMI_CONFIG}"
+BSSDBPASSWD="$(openssl rand -base64 32)"; sed -i'' -e "s@BSS_DB_PASSWD@$BSSDBPASSWD@" ${OCHAMI_CONFIG}
+HYDRADBPASSWD="$(openssl rand -base64 32)"; sed -i'' -e "s@HYDRA_DB_PASSWD@$HYDRADBPASSWD@" ${OCHAMI_CONFIG}
