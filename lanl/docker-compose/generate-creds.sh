5423d4657c1a8d63f70b1925eb16266626b5da7b
David J. Allen <allend@lanl.gov>
2024-02-16 18:16:00 -0700

    Fixed kratos not starting correctly

--- lanl/docker-compose/generate-creds.sh
+++ lanl/docker-compose/generate-creds.sh
@@ -4,4 +4,5 @@
 echo "POSTGRES_PASSWORD=$(openssl rand -base64 32)" > .env
 echo "BSS_POSTGRES_PASSWORD=$(openssl rand -base64 32)" >> .env
 echo "SMD_POSTGRES_PASSWORD=$(openssl rand -base64 32)" >> .env
-echo "HYDRA_POSTGRES_PASSWORD=$(openssl rand -base64 32)" >> .env
+echo "HYDRA_POSTGRES_PASSWORD=hydra" >> .env
+echo "KRATOS_POSTGRES_PASSWORD=kratos" >> .env
