8de21178334fb9d26f9a2e2f31cde93441525836
Alex Lovell-Troy <alovelltroy@lanl.gov>
2024-04-22 18:10:23 -0400

    Update configuration to allow for setting the system_name in env and reflect in certs

--- quickstart/generate-configs.sh
+++ quickstart/generate-configs.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# Ochami config file location
-OCHAMI_CONFIG=${OCHAMI_CONFIG:-configs/ochami-config.yaml}
-
-if [ -f $OCHAMI_CONFIG ]
+if [ -f .env ]
 then
 	echo "A config file exists. Delete to generate a new one"
 	exit 1
 fi
 
+if [ -z "$1" ]
+then
+	echo "Usage: $0 system-name"
+	exit 1
+fi
+
+# Set the system name
+echo "# This file is used by docker compose to set environment variables" > .env
+echo "# For more information about how it is read and how to override items in it, see the docs:" >> .env
+echo "#   https://docs.docker.com/compose/environment-variables/set-environment-variables/" >> .env
+
+# Set the system name which is used for certs
+echo "SYSTEM_NAME=$1" >> .env
 # Set DB passwords 
-echo "POSTGRES_PASSWORD=$(openssl rand -base64 32 | openssl dgst | cut -d' ' -f2)" > .env
+echo "POSTGRES_PASSWORD=$(openssl rand -base64 32 | openssl dgst | cut -d' ' -f2)" >> .env
 echo "BSS_POSTGRES_PASSWORD=$(openssl rand -base64 32 | openssl dgst | cut -d' ' -f2)" >> .env
 echo "SMD_POSTGRES_PASSWORD=$(openssl rand -base64 32 | openssl dgst | cut -d' ' -f2)" >> .env
 echo "HYDRA_POSTGRES_PASSWORD=$(openssl rand -base64 32 | openssl dgst | cut -d' ' -f2)" >> .env
-
-echo "OCHAMI_CONFIG=$OCHAMI_CONFIG" >> .env
