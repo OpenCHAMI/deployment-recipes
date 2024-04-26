1431d01ecae124964a57737f19bb5f85ba772691
Alex Lovell-Troy <alovelltroy@lanl.gov>
2024-04-26 10:10:05 -0400

    Update bash_functions to be useful for debugging
     source bash_functions.sh
     get_ca_cert > cacert.pem
     ACCESS_TOKEN=$(gen_access_token)
      curl --cacert cacert.pem -H "Authorization: Bearer $ACCESS_TOKEN" https://foobar.openchami.cluster/hsm/v2/State/Components

--- quickstart/bash_functions.sh
+++ quickstart/bash_functions.sh
@@ -16,26 +16,38 @@ get_ca_cert() {
 
 container_curl() {
     local url=$1
-    docker exec -it quay.io/curl/curl -s $url
+    docker exec -it --rm quay.io/curl/curl -s $url
 }
 
 create_client_credentials() {
    docker exec hydra hydra create client \
     --endpoint http://hydra:4445/ \
     --format json \
-    --grant-type client_credentials
+    --grant-type client_credentials \
+    --scope openid \
+    --scope smd.read 
 }
-# $(echo $CLIENT_TOKEN | jq -r '"\(.client_id):\(.client_secret)"')
+
+# CLIENT_CREDENTIALS=$(create_client_credentials)
+# $(echo $CLIENT_CREDENTIALS | jq -r '"\(.client_id):\(.client_secret)"')
 
 retrieve_access_token() {
     local CLIENT_ID=$1
     local CLIENT_SECRET=$2
 
-    docker run --network quickstart_jwt-internal quay.io/curl/curl:latest curl -s -u "$CLIENT_ID:$CLIENT_SECRET" \
+    docker run --rm --network quickstart_jwt-internal quay.io/curl/curl:latest curl -s -u "$CLIENT_ID:$CLIENT_SECRET" \
     -d grant_type=client_credentials \
-    -d scope=openid \
-    -d scope=smd.read \
+    -d scope=openid+smd.read \
     http://hydra:4444/oauth2/token
 }
 
 # ACCESS_TOKEN=$(retrieve_access_token $CLIENT_ID $CLIENT_SECRET | jq -r .access_token)
+
+gen_access_token() {
+    local CLIENT_CREDENTIALS
+    CLIENT_CREDENTIALS=$(create_client_credentials)
+    local CLIENT_ID=`echo $CLIENT_CREDENTIALS | jq -r '.client_id'`
+    local CLIENT_SECRET=`echo $CLIENT_CREDENTIALS | jq -r '.client_secret'`
+    local ACCESS_TOKEN=$(retrieve_access_token $CLIENT_ID $CLIENT_SECRET | jq -r .access_token)
+    echo $ACCESS_TOKEN
+}
