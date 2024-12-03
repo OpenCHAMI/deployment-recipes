8fb945820712ba13cf6feff61a3ce67a0824a111
Brian Friesen <bfriesen@lbl.gov>
2024-12-03 14:36:27 -0800

    lbnl, helm: remove unused krakend deployment
    krakend was used in the past in docker-compose recipes, but is not used any
    more in the quickstart guide, so let's remove it from the Helm charts too.

--- lbnl/helm/scripts/template.sh
+++ lbnl/helm/scripts/template.sh
@@ -10,7 +10,6 @@ helm template \
   --set init.job.image.repository="${repo}" \
   --set smd.deployment.image.repository="${repo}" \
   --set postgres.deployment.image.repository="${repo}" \
-  --set krakend.deployment.image.repository="${repo}" \
   --set dnsmasq.deployment.image.repository="${repo}" \
   --set hydra.deployment.image.repository="${repo}" \
   --set hydra_consent.deployment.image.repository="${repo}" \
