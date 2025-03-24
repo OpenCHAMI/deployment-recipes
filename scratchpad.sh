85247a718eb51f2585931e65508cda7bdcae9a71
yogi4 <yogi@yogi4.com>
2025-03-23 23:45:12 -0500

    Keylime Integration

--- scratchpad.sh
+++ scratchpad.sh
@@ -1,2 +1,4 @@
 sudo git pull origin 83-dev-integrate-keylime
 ansible-playbook -i inventory.yaml -l keylime_server -K site.yaml
+journalctl -u keylime_verifier.service -f
+journalctl -u keylime_registrar.service -f
