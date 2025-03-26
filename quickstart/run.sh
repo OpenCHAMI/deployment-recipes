e5139f49172a6e12bd0faa61c4ecd73c239e7f60
|°_°| <grivel.theo@protonmail.com>
2025-03-26 10:44:15 +0100

    script(run): start services and create token

---
+++ quickstart/run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+until docker-compose \
+  -f base.yml \
+  -f postgres.yml \
+  -f jwt-security.yml \
+  -f haproxy-api-gateway.yml \
+  -f  openchami-svcs.yml \
+  -f autocert.yml \
+  -f coredhcp.yml \
+  -f pcs.yml \
+  -f vault.yml \
+  -f etcd.yml \
+  -f configurator.yml up -d
+do
+docker-compose \
+  -f base.yml \
+  -f postgres.yml \
+  -f jwt-security.yml \
+  -f haproxy-api-gateway.yml \
+  -f  openchami-svcs.yml \
+  -f autocert.yml \
+  -f coredhcp.yml \
+  -f pcs.yml \
+  -f vault.yml \
+  -f etcd.yml \
+  -f configurator.yml down
+done
+
+source bash_functions.sh
+gen_access_token > access_token
