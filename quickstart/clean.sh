954b6fbbf70480e9ee30c019be02bea5ab29d511
|°_°| <grivel.theo@protonmail.com>
2025-04-04 10:40:05 +0200

    fix(clean): missing dollar and bad test

--- quickstart/clean.sh
+++ quickstart/clean.sh
@@ -4,4 +4,4 @@ rm -rf keys
 
 VOLUME=$(docker volume ls --format "{{.Name}}" --filter "name=quickstart")
 
-if [[ -z VOLUME ]]; then docker volume rm $VOLUME; fi
+if [ -n "${VOLUME}" ]; then docker volume rm "${VOLUME}"; fi
