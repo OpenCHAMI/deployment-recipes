4c0cf70a9194ce8e8b2360ca8085e5faab1e8030
|°_°| <grivel.theo@protonmail.com>
2025-04-08 13:33:10 +0200

    script(check) add error message

--- quickstart/check.sh
+++ quickstart/check.sh
@@ -2,5 +2,11 @@
 
 echo "check the if pcs is running"
 
-docker logs pcs 2>&1 | grep '**RUNNING -- Listening'
-docker logs pcs 2>&1 | grep 'ETCD connection succeeded.'
+check_pcs_logs() {
+	if ! docker logs pcs 2>&1 | grep --color "${1}" ; then
+		echo "error: pcs miss \"${1}\""
+	fi
+}
+
+check_pcs_logs '**RUNNING -- Listening'
+check_pcs_logs 'ETCD connection succeeded.'
