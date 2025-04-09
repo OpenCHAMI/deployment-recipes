dc5d84c9a6a4cf5f275902227f981538c41712dc
|°_°| <grivel.theo@protonmail.com>
2025-04-09 10:00:24 +0200

    script(check): list vault secret

--- quickstart/check.sh
+++ quickstart/check.sh
@@ -8,5 +8,14 @@ check_pcs_logs() {
 	fi
 }
 
+check_vault_list() {
+	for secret in $(docker exec -e VAULT_TOKEN=hms vault vault list --format json secret/hms-creds/ | jq -r  | tr -d '[],\n"'); do
+		echo $secret
+		docker exec -e VAULT_TOKEN=hms vault vault read secret/hms-creds/$secret
+	done
+}
+
 check_pcs_logs '**RUNNING -- Listening'
 check_pcs_logs 'ETCD connection succeeded.'
+
+check_vault_list
