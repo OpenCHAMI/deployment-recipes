d57771f0ab075dfce3669d14cd658ea1df1e2955
|°_°| <grivel.theo@protonmail.com>
2025-04-10 15:58:21 +0200

    script(populate-smd): just one rfe

--- quickstart/populate-smd.sh
+++ quickstart/populate-smd.sh
@@ -4,56 +4,8 @@
 # 1: https://github.com/OpenCHAMI/power-control/blob/main/docker-compose.test.ct.yaml#L108
 
 curl -X POST -d '{"RedfishEndpoints":[{
-  "ID":"x0c0b0",
-  "FQDN":"x0c0b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s0b0",
-  "FQDN":"x0c0s0b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s1b0",
-  "FQDN":"x0c0s1b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s2b0",
-  "FQDN":"x0c0s2b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s3b0",
-  "FQDN":"x0c0s3b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s4b0",
-  "FQDN":"x0c0s4b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s5b0",
-  "FQDN":"x0c0s5b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s6b0",
-  "FQDN":"x0c0s6b0",
-  "RediscoverOnUpdate":true,
-  "User":"root",
-  "Password":"root_password"
-},{
-  "ID":"x0c0s7b0",
-  "FQDN":"x0c0s7b0",
+  "ID":"x1000c0s0b3",
+  "FQDN":"x1000c0s0b3",
   "RediscoverOnUpdate":true,
   "User":"root",
   "Password":"root_password"
